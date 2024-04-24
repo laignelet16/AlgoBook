3a0e979c5c2283437881bb3bc1107200efe96d2a
jal1077 <jal1077@nyu.edu>
2024-04-24 00:48:35 -0400

    Added an example of queues being used in a game and a quick explanation of queues

--- java/queue/guess.java
+++ java/queue/guess.java
@@ -5,6 +5,8 @@ import java.util.Queue;
 import java.util.Scanner;
 import java.util.Random;
 
+// This is a small guessing game that uses linkedlists and queues
+
 public class guess {
     public static void main(String[] args) {
         guess();
@@ -18,8 +20,11 @@ public class guess {
         System.out.println("We are going to play a quick guessing gam \n");
         System.out.println("Guess a number between 1 and 100 \n");
 
+        // Scanner allows you to add inputs and for the system to recognize it
         Scanner scanner = new Scanner(System.in);
 
+        // uses a while loop to allow you to continue to guess the number until you get
+        // it
         while (true) {
             System.out.println("Guess the number");
             String input = scanner.nextLine();
@@ -33,6 +38,7 @@ public class guess {
 
             if (guess < secretNumber) {
                 System.out.println("Too low! Try again");
+                // This adds the guess to the line
                 previousGuesses.add(guess);
             } else if (guess > secretNumber) {
                 System.out.println("Too high! Try again");
